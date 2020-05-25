af6ce21c7742263d7f74d20b60c65e7942df20ce
powsokan <powsi1997@gmail.com>
2020-05-25 11:16:31 +0530

    auth setup

---
+++ sidebar/auth/new.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Library</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="Container">
+        <div id="header">
+             <h1>E-Library Management System</h1>
+        </div>  
+        <div id="wrapper">
+        </div> 
+        <div id="navi">
+           <?php
+           include "sidebar/sideBar.php";
+           ?>
+        </div>
+        <div id="footer">
+           <p>Copyright &copy; Powsikan 2020</p>
+        </div>
+    </div>   
+</body>
+</html>
