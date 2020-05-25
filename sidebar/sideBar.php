70cca5a700d18a6aa9b178db296df4dc589b025a
powsokan <powsi1997@gmail.com>
2020-05-25 11:18:39 +0530

    path correction

--- sidebar/sideBar.php
+++ sidebar/sideBar.php
@@ -1,6 +1,6 @@
 <ul>
-   <li><a href="alogin.php"> Admin Login</a></li>
-   <li><a href="ulogin.php"></a>User Login</li>
-   <li><a href="new.php"></a> New User</li>
+   <li><a href="./auth/alogin.php"> Admin Login</a></li>
+   <li><a href="./auth/ulogin.php"></a>User Login</li>
+   <li><a href="./auth/new.php"></a> New User</li>
 
  </ul>
