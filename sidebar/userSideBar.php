d71d456c0b3c58150da997235a372786ec43e33f
powsokan <powsi1997@gmail.com>
2020-05-25 11:14:03 +0530

    navigation setup

--- sidebar/userSideBar.php
+++ sidebar/userSideBar.php
@@ -1,7 +1,8 @@
 <ul>
-   <li><a href="#"></a></li>
-   <li><a href="#"></a></li>
-   <li><a href="#"></a></li>
-   <li><a href="#"></a></li>
+   <li><a href="uhome.php">Home</a></li>
+   <li><a href="search_books.php">Search Books</a></li>
+   <li><a href="request.php">Request</a></li>
+   <li><a href="uchagePass.php">Change Password</a></li>
+   <li><a href="logout.php">Logout</a></li>
 
  </ul>
