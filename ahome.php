953b8706b882fe60773713160012ba1f6e820a36
powsokan <powsi1997@gmail.com>
2020-05-28 19:43:24 +0530

    ahome created

--- ahome.php
+++ ahome.php
@@ -1,6 +1,45 @@
 <?php
 session_start();
+include  "database.php";
+
+function countRecord($sql,$db)
+{
+    $res=$db->query($sql);
+    return $res->num_rows;
+}
+
 if(!isset($_SESSION["AID"])){
     header("location:alogin.php");
 }
 ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Library</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="Container">
+        <div id="header">
+             <h1>E-Library Management System</h1>
+        </div>  
+        <div id="wrapper">
+            <h3 class="heading">Welcome Admin</h3>
+            <div class="center">
+            
+            </div>
+        </div> 
+        <div id="navi">
+           <?php
+           include "adminSideBar.php";
+           ?>
+        </div>
+        <div id="footer">
+           <p>Copyright &copy; Powsikan 2020</p>
+        </div>
+    </div>   
+</body>
+</html>
