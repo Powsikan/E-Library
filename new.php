6c60e026f12eb70a7e434bfe87026c1645c1ca65
powsokan <powsi1997@gmail.com>
2020-05-29 17:37:39 +0530

    user registration

--- new.php
+++ new.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+include  "database.php";
+
+
+
+if(!isset($_SESSION["AID"])){
+    header("location:alogin.php");
+}
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,15 +22,32 @@
              <h1>E-Library Management System</h1>
         </div>  
         <div id="wrapper">
-        <h3 class="heading">New User Register Here</h3>
+        <h3 class="heading">New User Register</h3>
             <div class="center">
-            <form action="">
+            <?php
+                    if(isset($_POST["submit"])){
+                            $s="insert into student(NAME,PASS,MAIL,DEP) values ('{$_POST["uname"]}','{$_POST["upass"]}','{$_POST["mail"]}','{$_POST["dep"]}')";
+                            $db->query($s);
+                            echo "<p class='success'>User Registration Success</p>";
+                    }
+
+                ?>
+
+
+            <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
                 <label for="">Name</label>
                 <input type="text" name="uname" required>
                 <label for="">Email</label>
-                <input type="text" name="umail" required>
+                <input type="email" name="mail" required>
                 <label for="">Password</label>
                 <input type="password" name="upass" required>
+                <select name="dep" id="">
+                <option value="">Select</option>
+                <option value="SE">SE</option>
+                <option value="MIT">MIT</option>
+                <option value="ENCM">ENCM</option>
+                <option value="PS">PS</option>
+                </select>
                 <button type="submit">Login Now</button>
             </form>
             </div>
