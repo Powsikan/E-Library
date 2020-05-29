025048a4019383e799b494727d99b1f8f7b93ee2
powsokan <powsi1997@gmail.com>
2020-05-29 12:27:36 +0530

    comment

--- view_comm.php
+++ view_comm.php
@@ -23,17 +23,18 @@ if(!isset($_SESSION["AID"])){
              <h1>E-Library Management System</h1>
         </div>  
         <div id="wrapper">
-            <h3 class="heading">View Request Details</h3>
+            <h3 class="heading">View Comment Details</h3>
            
             <?php
-            $sql="select s.name,r.mes,r.logs from student s inner join request r on s.id=r.id";
+            $sql="select b.btitle,s.name,c.comm,c.logs from  comment c inner join book b on b.bid=c.bid inner join student s on c.sid=s.id";
             $res=$db->query($sql);
             if($res->num_rows>0){
                 echo "<table>
                     <tr>
                        <th>SNO</th>
-                       <th>NAME</th>
-                       <th>MESSAGE</th>
+                       <th>BOOK NAME</th>
+                       <th>STUDENT NAME</th>
+                       <th>COMMENT</th>
                        <th>DATE</th>
                     </tr>
                 ";
@@ -42,14 +43,15 @@ if(!isset($_SESSION["AID"])){
                     $i++; 
                     echo "<tr>";
                     echo "<td>{$i}</td>";
+                    echo "<td>{$row["btitle"]}</td>";
                     echo "<td>{$row["name"]}</td>";
-                    echo "<td>{$row["mes"]}</td>";
+                    echo "<td>{$row["comm"]}</td>";
                     echo "<td>{$row["logs"]}</td>";
                     echo "</tr>";
                 }
                 echo "</table>";
             }else{
-                echo "<p class='error'>No Request Record Found</p>";
+                echo "<p class='error'>No Comment Found</p>";
             }
             ?>
            
