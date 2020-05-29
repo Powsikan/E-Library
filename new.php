7b7a8097aa02420b67be3cc574c2bf27524b010e
powsokan <powsi1997@gmail.com>
2020-05-29 18:06:32 +0530

    bug fixed

--- new.php
+++ new.php
@@ -42,7 +42,7 @@ include  "database.php";
                 <option value="ENCM">ENCM</option>
                 <option value="PS">PS</option>
                 </select>
-                <button type="submit">Register Now</button>
+                <button type="submit" name="submit">Register Now</button>
             </form>
             </div>
         </div> 
